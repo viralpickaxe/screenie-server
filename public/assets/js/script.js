2124653483d4e2d929aa944d0e7c2ed64ecdbbc5
Jamie Davies <viralpickaxe@gmail.com>
2014-09-23 15:26:40 +0100

    Core screenshot server and api

---
+++ public/assets/js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  $('.js-createscreenshot').find('.js-url').focus();
+})
+
+$('.js-createscreenshot').on('submit',function(){
+  var $form = $(this);
+  var $url = $form.find('.js-url');
+  var $btn = $form.find('.js-btn');
+  var oldbtn = $btn.html();
+  
+  $url.prop('disabled',true);
+  $btn.html('Queuing').prop('disabled',true);
+
+  $.ajax({
+    type: "POST",
+    data: JSON.stringify({url:$url.val()}),
+    contentType: 'application/json',
+    url: "/screenshot/",
+    success: function(response){
+      if(response.success){
+        $url.val('').prop('disabled',false);
+        $btn.html(oldbtn).prop('disabled',false);
+        $form.find('.viewlink').prop('href',response.imgurl).show();;
+      } else {
+        $url.prop('disabled',false);
+        $btn.html(oldbtn).prop('disabled',false);
+        notification_show(response.msg,'error',5000);
+      }
+    }
+  });
+
+  return false;
+});
+
+function notification_show(notification,type,time){
+  if($('.notification').length==0){
+    $('body').prepend($('<div class="notification"></div>'));
+  } else {
+    clearTimeout(NotificationTimeout);
+  }
+  var $notif = $('.notification');
+  var time = time || 3000;
+
+  $notif.html(notification);
+  $notif.removeClass('success error').addClass(type).show('slide',{direction:'up'});
+  window.NotificationTimeout = setTimeout(function(){
+    notification_hide();
+  },time);
+}
+
+function notification_hide(){
+  var $notif = $('.notification');
+
+  $notif.hide('slide',{direction:'up'},function(){
+    $notif.remove();
+  });
+}
---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var http = require('http');
+var socket = require('socket.io');
+var path = require('path');
+var swig  = require('swig');
+var screenshot = require('url-to-image');
+
+var port = Number(process.env.PORT || 5000);
+
+var app = express();
+var server = http.Server(app);
+var io = socket(server).listen(server);
+
+app.engine('html', swig.renderFile);
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'html');
+app.set('views', './views');
+
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+
+server.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+app.get('/', function(req,res){
+  res.render('index', {});
+});
+
+app.post('/screenshot/', function(req,res){
+  var url = req.param('url');
+  if(url!=''){
+    var failed = false;
+    var imgurl = '/screenshots/' + getTime() + '.png';
+    screenshot(url,'public' + imgurl).fail(function(err){
+      failed = true;
+      res.send({success:false,msg:"Invalid URL"});
+    }).done(function(){
+      if(!failed){
+        console.log('screenshotted: ' + url + ' >>> ' + imgurl);
+        res.send({success:true,imgurl:imgurl});
+      }
+    });
+  } else {
+    res.send({success:false,msg:"Invalid URL"});
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//
+
+var getTime = function(){
+  var d = new Date();
+  var time = d.getTime();
+  return time;
+}
+
+var gracefulShutdown = function() {
+  console.log("Received kill signal, shutting down gracefully.");
+  server.close();
+  console.log("Closed out remaining connections. Gracefully Shutting Down");
+  process.exit();
+}
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);  
